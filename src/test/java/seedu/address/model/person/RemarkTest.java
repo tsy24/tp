9bca32f38d6d02ad116a83791a58f78edaebf27e
tsy24 <szeying3@gmail.com>
2021-09-22 23:49:03 +0800

    Update test classes

--- src/test/java/seedu/address/model/person/RemarkTest.java
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -10,4 +12,25 @@ public class RemarkTest {
         assertThrows(NullPointerException.class, () -> new Address(null));
     }
 
+    @Test
+    public void equals() {
+        Remark remark = new Remark("Hello");
+
+        // same object -> returns true
+        assertTrue(remark.equals(remark));
+
+        // same values -> returns true
+        Remark remarkCopy = new Remark(remark.value);
+        assertTrue(remark.equals(remarkCopy));
+
+        // different types -> returns false
+        assertFalse(remark.equals(1));
+
+        // null -> returns false
+        assertFalse(remark.equals(null));
+
+        // different remark -> returns false
+        Remark differentRemark = new Remark("Bye");
+        assertFalse(remark.equals(differentRemark));
+    }
 }
