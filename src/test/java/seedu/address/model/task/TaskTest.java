50c2ca09386feb8fb3d856325bad6fe2ff42af61
Xiong Jingya <66959387+xiongjya@users.noreply.github.com>
2021-10-08 22:11:08 +0800

    Sorts tasks added to the list automatically by the date and time

--- src/test/java/seedu/address/model/task/TaskTest.java
+++ src/test/java/seedu/address/model/task/TaskTest.java
@@ -9,8 +9,7 @@ import static seedu.address.logic.commands.TaskCommandTestUtil.VALID_NAME_ALEX;
 import static seedu.address.logic.commands.TaskCommandTestUtil.VALID_NAME_KEITH;
 import static seedu.address.logic.commands.TaskCommandTestUtil.VALID_TIME_SEVENPM;
 import static seedu.address.logic.commands.TaskCommandTestUtil.VALID_TIME_TENAM;
-import static seedu.address.testutil.TypicalTasks.ALEX_INSULIN;
-import static seedu.address.testutil.TypicalTasks.KEITH_INSULIN;
+import static seedu.address.testutil.TypicalTasks.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -56,5 +55,21 @@ public class TaskTest {
         // different address -> returns false
         editedTask = new TaskBuilder(keithInsulin).withDesc(VALID_DESC_PAPERWORK).build();
         assertFalse(keithInsulin.equals(editedTask));
+    }
+
+    @Test
+    void compareTo() {
+        Task keithInsulin = new TaskBuilder(KEITH_INSULIN).build(); // date = "2020-11-01"
+        Task alexInsulin = new TaskBuilder(ALEX_INSULIN).build(); // date = "2022-01-31", time: "19:45" in 24 hrs time
+        Task doPaperwork = new TaskBuilder(DO_PAPERWORK).build(); // date: "2022-01-31", time: "10:20" in 24 hrs time
+
+        // keithInsulin before alexInsulin -> returns negative value
+        assertTrue(keithInsulin.compareTo(alexInsulin) < 0);
+
+        // alexInsulin after doPaperwork -> returns positive value
+        assertTrue(alexInsulin.compareTo(doPaperwork) > 0);
+
+        // doPaperwork before alexInsulin -> returns negative value
+        assertFalse(doPaperwork.compareTo(keithInsulin) == 0);
     }
 }
