188138fad8cb0976dffc0cbeb15e43732fac49c0
Xiong Jingya <66959387+xiongjya@users.noreply.github.com>
2021-10-19 02:46:08 +0800

    Edit the use of word for standardisation

---
+++ src/main/java/seedu/address/logic/commands/FindTaskCommand.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands;
+
+/**
+ * Finds and lists all tasks in the address book which description contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FindTaskCommand {
+
+}
